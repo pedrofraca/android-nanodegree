0c4f4497dc49e6df01033f8582d48bb769e1b97b
pedrofraca <pedro.fraca@gmail.com>
2015-10-24 23:45:59 +0200

    Check null pointer to avoid crash when the device is rotated

--- superduo/alexandria/app/src/main/java/it/jaschke/alexandria/BookDetail.java
+++ superduo/alexandria/app/src/main/java/it/jaschke/alexandria/BookDetail.java
@@ -100,7 +100,10 @@ public class BookDetail extends Fragment implements LoaderManager.LoaderCallback
         shareIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_DOCUMENT);
         shareIntent.setType("text/plain");
         shareIntent.putExtra(Intent.EXTRA_TEXT, getString(R.string.share_text) + volumeInfo.title());
-        shareActionProvider.setShareIntent(shareIntent);
+        if(shareActionProvider!=null){
+            shareActionProvider.setShareIntent(shareIntent);
+        }
+
 
         ((TextView) rootView.findViewById(R.id.fullBookSubTitle)).setText(volumeInfo.subtitle());
         ((TextView) rootView.findViewById(R.id.fullBookDesc)).setText(volumeInfo.description());
